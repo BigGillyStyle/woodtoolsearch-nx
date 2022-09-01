caa937d151c7b830511194b62b421dea1d9b7ba2
Andy Pickler <andy.pickler@gmail.com>
2022-09-01 06:56:53 -0600

    Add Sanity webhook

--- apps/api/src/hooks/sanity/bandsaw/bandsaw.module.ts
+++ apps/api/src/hooks/sanity/bandsaw/bandsaw.module.ts
@@ -1,9 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { BandsawService } from './bandsaw.service';
 import { BandsawController } from './bandsaw.controller';
+import { requireSignedRequest } from '@sanity/webhook';
 
 @Module({
   controllers: [BandsawController],
   providers: [BandsawService],
 })
-export class BandsawModule {}
+export class BandsawModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(requireSignedRequest({ secret: process.env.SANITY_WEBHOOK_SECRET, parseBody: true }))
+      .forRoutes({ path: 'bandsaw', method: RequestMethod.POST });
+  }
+}
